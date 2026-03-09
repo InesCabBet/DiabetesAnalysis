2f0042550bad77dee1b67524b8bc15c7a84f4ba0
InesCabreraBetancor <icb3rg@gmail.com>
2026-03-09 16:44:20 +0000

    intro

---
+++ intro.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Mi Proyecto Final: Predicción de Diabetes")
+
+st.markdown("""
+# Modelado de patrón de pacientes con diabetes.
+Esta aplicación tiene como objetivo poder modelar diagnósticos de diabetes en pacientes
+usando distintas técnicas de ML
+            
+**Navegación** En el menú de la izquierda tienes todo lo necesario para empezar a predecir si el paciente tiene diabetes o no
+""")
