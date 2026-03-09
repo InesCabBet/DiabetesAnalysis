a2835c401dfd7b943892fd8250395801727d8396
InesCabreraBetancor <icb3rg@gmail.com>
2026-03-09 17:00:49 +0000

    change intro

--- intro.py
+++ intro.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.title("Mi Proyecto Final: Predicción de Diabetes")
 
 st.markdown("""
-# Modelado de patrón de pacientes con diabetes.
-Esta aplicación tiene como objetivo poder modelar diagnósticos de diabetes en pacientes
-usando distintas técnicas de ML
+# El objetivo del proyecto es desarrollar un modelo de machine learning capaz de predecir si 
+un paciente tiene probabilidad de padecer diabetes basándose en indicadores médicos como glucosa, 
+IMC, edad o presión arterial.
             
 **Navegación** En el menú de la izquierda tienes todo lo necesario para empezar a predecir si el paciente tiene diabetes o no
 """)
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 pg_intro = st.Page("intro.py", title="Introducción")
+
 navigation_env = st.navigation(
     {
         "": [pg_intro]
