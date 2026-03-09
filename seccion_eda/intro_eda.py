a50eb58b015c9b7793613ce25a74be82ec1c0e05
InesCabreraBetancor <icb3rg@gmail.com>
2026-03-09 17:17:11 +0000

    avances

---
+++ seccion_eda/intro_eda.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("Análisis Exploratorio de Datos: Introducción")
+
+st.markdown("""
+El objetivo de esta sección es entender los datos, qué nos cuentan
+Limpiar los datos en caso de ruido, para que el modelo tenga datos relevantes
+""")
+
+pd.read_csv("diabetes.csv")
