ff167a4106740a2a571adf04cd353a54b4e8e558
InesCabreraBetancor <icb3rg@gmail.com>
2026-03-09 17:01:17 +0000

    intro

--- intro.py
+++ intro.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Mi Proyecto Final: Predicción de Diabetes")
 
 st.markdown("""
-# El objetivo del proyecto es desarrollar un modelo de machine learning capaz de predecir si 
+El objetivo del proyecto es desarrollar un modelo de machine learning capaz de predecir si 
 un paciente tiene probabilidad de padecer diabetes basándose en indicadores médicos como glucosa, 
 IMC, edad o presión arterial.
             
