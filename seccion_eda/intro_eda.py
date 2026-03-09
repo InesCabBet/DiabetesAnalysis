b8977a51384ac6a4502cdc875e8fc38474877315
InesCabreraBetancor <icb3rg@gmail.com>
2026-03-09 17:22:23 +0000

    datos analizados

--- seccion_eda/intro_eda.py
+++ seccion_eda/intro_eda.py
@@ -6,9 +6,17 @@ import seaborn as sns
 st.title("Análisis Exploratorio de Datos: Introducción")
 
 st.markdown("""
-El objetivo de esta sección es entender los datos, qué nos cuentan
+El objetivo de esta sección es entender los datos, qué nos cuentan.
 Limpiar los datos en caso de ruido, para que el modelo tenga datos relevantes
 """)
 
 df = pd.read_csv("diabetes.csv")
-st.dataframe(df.head())
+st.dataframe(df.head(10))
+
+st.markdown("""
+Podemos observar que hay datos a 0 pero que es fisiológicamente imposible que lo estén. 
+Entiendo que son datos faltantes, es por ello que cambiaré dichos datos por la mediana en las columnas afectadas, que son:
+            1. Glucose 
+            2. Insulin
+            3. BMI
+            4. BloodPresure""")
